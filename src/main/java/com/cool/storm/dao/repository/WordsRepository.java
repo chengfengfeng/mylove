ef93ff81b49aac2e3b5492f1be5949bf066802e0
chengzhangheng <chengzhangheng@d-chance.cn>
2019-06-27 18:32:31 +0800

    Revert "1"
    This reverts commit 9fed69855871636f4e8b88dd1171b10b6526b918.

--- src/main/java/com/cool/storm/dao/repository/WordsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cool.storm.dao.repository;
-
-import com.cool.storm.dao.entity.Words;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WordsRepository extends JpaRepository<Words, Integer> {
-}
